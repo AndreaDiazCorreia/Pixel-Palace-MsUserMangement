b00a72127b8a159516c7ba3a0a70f82e8ffcd37a
Jscm15 <85587175+Jscm15@users.noreply.github.com>
2023-11-05 20:23:38 -0500

    Update method added

--- msUserManagement/src/main/java/com/pixelpalace/msUserManagement/service/PixelUserService.java
+++ msUserManagement/src/main/java/com/pixelpalace/msUserManagement/service/PixelUserService.java
@@ -7,4 +7,6 @@ public interface PixelUserService {
     void postUser(PixelUser pixelUser) throws Exception;
 
     ResponseEntity<Object> deleteUser(long id);
+
+     PixelUser update(Long id, PixelUser user);
 }
